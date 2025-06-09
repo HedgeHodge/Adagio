c5bfcae007b81d9f93753cee40458815351e9bae
Tyler Hodges <hodges3902@gmail.com>
2025-06-09 15:26:34 +0000

    My api key is AIzaSyCFv_htOY59P8puWNrQnIJnkhXTaf2sVbk

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI()],
+  plugins: [googleAI({apiKey: "AIzaSyCFv_htOY59P8puWNrQnIJnkhXTaf2sVbk"})],
   model: 'googleai/gemini-2.0-flash',
 });
