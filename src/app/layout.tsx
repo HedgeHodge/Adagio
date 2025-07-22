abd2a55b426a14f2defe1198a4a0784b9e79bfde
Tyler Hodges <hodges3902@gmail.com>
2025-07-22 21:44:33 +0000

    Lets use Domine actually

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
 
 import type { Metadata, Viewport } from 'next';
-import { Poppins, Pacifico } from 'next/font/google';
+import { Domine, Pacifico } from 'next/font/google';
 import './globals.css';
 import { Toaster } from "@/components/ui/toaster";
 import { Providers } from '@/components/layout/Providers';
 import { useId } from 'react';
 
-const poppins = Poppins({
+const domine = Domine({
   subsets: ['latin'],
   display: 'swap',
   weight: ['400', '500', '600', '700'],
@@ -53,7 +53,7 @@ export default function RootLayout({
 }>) {
   const id = useId();
   return (
-    <html lang="en" className={`${poppins.variable} ${pacifico.variable}`} suppressHydrationWarning>
+    <html lang="en" className={`${domine.variable} ${pacifico.variable}`} suppressHydrationWarning>
       <body className="font-body antialiased">
         <Providers key={id}>
             {children}
