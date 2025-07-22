9fb5df76b97deabf3cf8a64692b0c3eb3a0118d0
Tyler Hodges <hodges3902@gmail.com>
2025-07-22 23:58:43 +0000

    It didn't fade out slow enough

--- src/components/layout/SplashScreen.tsx
+++ src/components/layout/SplashScreen.tsx
@@ -8,7 +8,7 @@ export const SplashScreen = () => (
         className="fixed inset-0 z-50 flex items-center justify-center bg-transparent"
         initial={{ opacity: 1 }}
         exit={{ opacity: 0 }}
-        transition={{ duration: 0.5, ease: 'easeInOut' }}
+        transition={{ duration: 1.0, ease: 'easeInOut' }}
     >
         <motion.div
             initial={{ opacity: 0, scale: 0.9 }}
