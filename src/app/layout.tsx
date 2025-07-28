10d3346eb14436789dd0e4e3b7cb47277f3a6136
Tyler Hodges <hodges3902@gmail.com>
2025-07-28 02:17:07 +0000

    Fix these errors

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,10 @@ export const metadata: Metadata = {
     telephone: false,
   },
   icons: {
+    icon: [
+      { url: '/icons/icon-192x192.png', sizes: '192x192', type: 'image/png' },
+      { url: '/icons/icon-512x512.png', sizes: '512x512', type: 'image/png' },
+    ],
     apple: '/appstore.png',
   },
 };
